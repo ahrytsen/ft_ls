fff45fbef456b36cc678fe1e9f301f1775a9b40a
Artur HRYTSENKO <ahrytsen@e2r13p10.unit.ua>
2018-01-03 21:00:24 +0200

    start

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/30 19:10:30 by ahrytsen          #+#    #+#             */
-/*   Updated: 2018/01/02 21:43:26 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2018/01/03 15:43:16 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,12 @@
 # include "libft.h"
 # include <dirent.h>
 # include <stdio.h>
+# include <sys/errno.h>
+
+# define FT_LFRMT 0x1
+# define FT_RECURS 0x2
+# define FT_ALL 0x4
+# define FT_REV 0x8
+# define FT_TIME_M 0x10
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/ft_ls.c
+++ src/ft_ls.c
@@ -6,13 +6,44 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/30 14:21:17 by ahrytsen          #+#    #+#             */
-/*   Updated: 2018/01/02 21:43:24 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2018/01/03 20:36:09 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	ft_print_info(struct dirent f_info, t_flg flags)
+static void	ft_usage(char *p_name, char tmp)
+{
+	ft_printf("%s: illegal option -- %c\n", p_name, tmp);
+	ft_printf("usage: ft_ls [-Ralrt1] [file ...]\n");
+	exit(errno);
+}
+
+static char	ft_get_flags(char *fl, uint64_t *flags)
+{
+	while (*fl)
+	{
+		if (*fl == 'l')
+			*flags |= FT_LFRMT;
+		else if (*fl == 'R')
+			*flags |= FT_RECURS;
+		else if (*fl == 'a')
+			*flags |= FT_ALL;
+		else if (*fl == 'r')
+			*flags |= FT_REV;
+		else if (*fl == 't')
+			*flags |= FT_TIME_M;
+		else
+		{
+			errno = EIO;
+			return (*fl);
+		}
+		fl++;
+	}
+	return (0);
+}
+
+/*void	ft_print_info(struct dirent f_info, uint64_t flags)
 {
 
 }
@@ -21,22 +52,20 @@ void	ft_ls(char	*path, int show_path)
 {
 
 }
-
+*/
 int	main(int ac, char **av)
 {
 	int				i;
-	DIR				*dir;
-	struct dirent	*dp;
-	char			*flags;
+//	DIR				*dir;
+//	struct dirent	*dp;
+	uint64_t		flags;
+	char			tmp;
 
-	i = 0;
-	flags = "";
-	if (ac > 1 && *av[1] == '-')
-	{
-		flags = *av[1];
-		i++;
-	}
-	while (i < ac)
+	i = 1;
+	flags = 0;
+	if (ac > 1 && *av[1] == '-' && i++)
+		(tmp = ft_get_flags(av[1] + 1, &flags)) ? ft_usage(av[0], tmp) : 0;
+/*	while (i < ac)
 	{
 		if (!(dir = opendir(ac == 1 ? "." : av[i + 1])))
 			perror(ac == 1 ? "." : av[i + 1]);
@@ -47,5 +76,5 @@ int	main(int ac, char **av)
 				ft_printf("%s\n", dp->d_name);
 		i++;
 		dir ? closedir(dir) : 0;
-	}
+		}*/
 }
